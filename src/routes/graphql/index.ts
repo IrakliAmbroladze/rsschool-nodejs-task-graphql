3af548f580e902c64ee0fcafa4a1e25a7f040c80
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-11-24 02:17:58 +0400

    feat: add depth limit validation

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,15 +1,19 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { buildSchema, graphql } from 'graphql';
+import { graphql, GraphQLSchema, validate, parse } from 'graphql';
 import { createDataLoaders } from './loaders.js';
+import depthLimit from 'graphql-depth-limit';
+import { RootQueryType } from './schema/queries.js';
+import { MutationsType } from './schema/mutations.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
-  const schema = buildSchema(`
-  type Query {
-    users: String
-  }
-`);
+
+  const schema = new GraphQLSchema({
+    query: RootQueryType,
+    mutation: MutationsType,
+  });
+
   fastify.route({
     url: '/',
     method: 'POST',
@@ -23,9 +27,33 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       const dataLoaders = await createDataLoaders(prisma);
       const context = {
         prisma,
-        ...{ dataLoaders },
+        ...dataLoaders,
       };
 
+      try {
+        const documentAST = parse(req.body.query);
+        const validationErrors = validate(schema, documentAST, [depthLimit(5)]);
+
+        if (validationErrors.length > 0) {
+          return {
+            data: null,
+            errors: validationErrors.map((err) => ({
+              message: err.message,
+              locations: err.locations,
+              path: err.path,
+            })),
+          };
+        }
+      } catch (error) {
+        return {
+          data: null,
+          errors: [
+            {
+              message: error instanceof Error ? error.message : error,
+            },
+          ],
+        };
+      }
       return graphql({
         schema,
         source: req.body.query,
